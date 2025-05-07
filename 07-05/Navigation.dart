99748b0860e38b9a461dcf6d50222ed7dc42d33c
bharath8341 <155081393+bharath8341@users.noreply.github.com>
2025-05-07 11:07:03 +0530

    Create Navigation.dart

---
+++ 07-05/Navigation.dart
@@ -0,0 +1,103 @@
+//main.dart
+
+import 'package:flutter/material.dart';
+import 'page1.dart';
+import 'page2.dart';
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const IndexPage(),
+      routes: {
+        '/page1': (context) => const Page1(),
+        '/page2': (context) => const Page2(),
+      },
+    );
+  }
+}
+
+class IndexPage extends StatelessWidget {
+  const IndexPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Index Page'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).pushNamed('/page1'),
+                child: const Text('Go to Page 1'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).pushNamed('/page2'),
+                child: const Text('Go to Page 2'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+//page1.dart
+
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page 1'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('Go Back'),
+        ),
+      ),
+    );
+  }
+}
+
+//page2.dart
+
+import 'package:flutter/material.dart';
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page 2'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('Go Back'),
+        ),
+      ),
+    );
+  }
+}
